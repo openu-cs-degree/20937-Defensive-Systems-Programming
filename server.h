4d2e14d70f6cc213b9baedbbbd05507ccabdec90
yonisimian <yonisimian@email.com>
2024-01-23 21:28:27 +0200

    implement read_request

--- server.h
+++ server.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <thread>
+#include <optional>
 
 #include "common.h"
 
@@ -41,23 +42,75 @@ namespace maman14
     boost::asio::io_context io_context;
     tcp::acceptor acceptor;
 
-    static void handle_client(tcp::socket socket)
+    static std::optional<Request> read_request(tcp::socket &socket)
     {
-      std::array<char, 128> buf;
+      std::cout << "read_request\n";
       boost::system::error_code error;
 
-      [[maybe_unused]] size_t len = socket.read_some(boost::asio::buffer(buf), error);
+      // Read the fixed-size part of the request
+      Request request;
+      boost::asio::read(socket, boost::asio::buffer(&request, offsetof(Request, filename)), error);
 
-      if (!error)
+      if (error)
       {
-        std::cout << "Deciphered!" << std::endl;
-        boost::asio::write(socket, boost::asio::buffer("Response from server"));
+        std::cout << "Error reading name_len: " << error.message() << '\n';
+        return std::nullopt;
+      }
+      request.name_len -= 11567; // TEMP: putty's connection sets name_len to 11568
+
+      // Read the filename
+      request.filename = std::make_unique<char[]>(request.name_len);
+      boost::asio::read(socket, boost::asio::buffer(request.filename.get(), request.name_len), error);
+
+      if (error)
+      {
+        std::cout << "Error reading filename: " << error.message() << '\n';
+        return std::nullopt;
+      }
+
+      // Read the size of the payload
+      boost::asio::read(socket, boost::asio::buffer(&request.payload.size, sizeof(request.payload.size)), error);
+
+      if (error)
+      {
+        std::cout << "Error: " << error.message() << '\n';
+        return std::nullopt;
+      }
+      request.payload.size -= 1498698868; // TEMP: putty's connection sets name_len to 1498698869
+
+      // Read the payload
+      request.payload.payload = std::make_unique<uint8_t[]>(request.payload.size);
+      boost::asio::read(socket, boost::asio::buffer(request.payload.payload.get(), request.payload.size), error);
+
+      if (error)
+      {
+        std::cout << "Error: " << error.message() << '\n';
+        return std::nullopt;
+      }
+
+      return request;
+    }
+
+    static void handle_client(tcp::socket socket)
+    {
+      if (auto request = read_request(socket))
+      {
+        std::cout << "Request received\n";
+        // std::cout << "user_id: " << request->user_id << '\n';
+        // std::cout << "version: " << request->version << '\n';
+        // std::cout << "op: " << static_cast<uint16_t>(request->op) << '\n';
+        // std::cout << "name_len: " << request->name_len << '\n';
+        // std::cout << "filename: " << request->filename.get() << '\n';
+        // std::cout << "payload size: " << request->payload.size << '\n';
+        // std::cout << "payload: " << request->payload.payload.get() << '\n';
       }
       else
       {
-        std::cout << "Error: " << error.message() << std::endl;
+        std::cout << "Request reading failed!" << '\n';
       }
+
+      boost::asio::write(socket, boost::asio::buffer("Response from server"));
     }
   };
 
-}
+} // namespace maman14
