1f5e8ea440e5854ce837265b908db17015d51d6a
yonisimian <yonisimian@email.com>
2024-01-24 01:47:27 +0200

    create dummy process_request and write_response

--- server.h
+++ server.h
@@ -13,6 +13,9 @@ namespace maman14
   class Server
   {
   public:
+    static constexpr uint8_t VERSION = 1;
+
+  public:
     Server(uint16_t port)
         : acceptor(io_context, tcp::endpoint(tcp::v4(), port))
     {
@@ -91,25 +94,115 @@ namespace maman14
       return request;
     }
 
-    static void handle_client(tcp::socket socket)
+    static std::optional<Response> process_request(Request &request)
     {
-      if (auto request = read_request(socket))
+      std::cout << "process_request\n";
+      Response response;
+      response.version = Server::VERSION;
+      response.name_len = request.name_len;
+      response.filename = std::move(request.filename);
+      response.payload = std::move(request.payload);
+
+      switch (request.op)
       {
-        std::cout << "Request received\n";
-        // std::cout << "user_id: " << request->user_id << '\n';
-        // std::cout << "version: " << request->version << '\n';
-        // std::cout << "op: " << static_cast<uint16_t>(request->op) << '\n';
-        // std::cout << "name_len: " << request->name_len << '\n';
-        // std::cout << "filename: " << request->filename.get() << '\n';
-        // std::cout << "payload size: " << request->payload.size << '\n';
-        // std::cout << "payload: " << request->payload.payload.get() << '\n';
-      }
-      else
-      {
-        std::cout << "Request reading failed!" << '\n';
+      case Op::SAVE:
+        response.status = Status::SUCCESS_SAVE;
+        break;
+      case Op::RESTORE:
+        response.status = Status::SUCCESS_RESTORE;
+        break;
+      case Op::DELETE:
+        response.status = Status::SUCCESS_RESTORE;
+        break;
+      case Op::LIST:
+        response.status = Status::SUCCESS_LIST;
+        break;
+      default:
+        response.status = Status::ERROR_GENERAL;
+        break;
       }
 
-      boost::asio::write(socket, boost::asio::buffer("Response from server"));
+      return response;
+    }
+
+    static void write_response(tcp::socket &socket, Response &response)
+    {
+      std::cout << "write_response\n";
+      boost::system::error_code error;
+
+      // Write the fixed-size part of the response
+      boost::asio::write(socket, boost::asio::buffer(&response, offsetof(Response, filename)), error);
+
+      if (error)
+      {
+        std::cout << "Error writing name_len: " << error.message() << '\n';
+        return;
+      }
+
+      // Write the filename
+      boost::asio::write(socket, boost::asio::buffer(response.filename.get(), response.name_len), error);
+
+      if (error)
+      {
+        std::cout << "Error writing filename: " << error.message() << '\n';
+        return;
+      }
+
+      // Write the size of the payload
+      boost::asio::write(socket, boost::asio::buffer(&response.payload.size, sizeof(response.payload.size)), error);
+
+      if (error)
+      {
+        std::cout << "Error: " << error.message() << '\n';
+        return;
+      }
+
+      // Write the payload
+      boost::asio::write(socket, boost::asio::buffer(response.payload.payload.get(), response.payload.size), error);
+
+      if (error)
+      {
+        std::cout << "Error: " << error.message() << '\n';
+        return;
+      }
+
+      std::cout << "Response sent\n";
+    }
+
+    static void handle_client(tcp::socket socket)
+    {
+      auto request = read_request(socket);
+      if (!request)
+      {
+        std::cout << "Request reading failed!" << '\n';
+        return;
+      }
+
+      std::cout << "Request received\n";
+      // std::cout << "user_id: " << request->user_id << '\n';
+      // std::cout << "version: " << request->version << '\n';
+      // std::cout << "op: " << static_cast<uint16_t>(request->op) << '\n';
+      // std::cout << "name_len: " << request->name_len << '\n';
+      // std::cout << "filename: " << request->filename.get() << '\n';
+      // std::cout << "payload size: " << request->payload.size << '\n';
+      // std::cout << "payload: " << request->payload.payload.get() << '\n';
+
+      auto respone = process_request(request.value());
+      if (!respone)
+      {
+        std::cout << "Request processing failed!" << '\n';
+        return;
+      }
+
+      std::cout << "Response ready\n";
+      // std::cout << "version: " << respone->version << '\n';
+      // std::cout << "status: " << static_cast<uint16_t>(respone->status) << '\n';
+      // std::cout << "name_len: " << respone->name_len << '\n';
+      // std::cout << "filename: " << respone->filename.get() << '\n';
+      // std::cout << "payload size: " << respone->payload.size << '\n';
+      // std::cout << "payload: " << respone->payload.payload.get() << '\n';
+
+      write_response(socket, respone.value());
     }
   };
 
